ec51d18af7518421026fc44d4637cb66b3fc5bbe
Gaikwad Kunal <118621248+dor3Monbotz@users.noreply.github.com>
2023-10-18 01:54:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
 from pyrogram.types import User, ChatMember
 from .config import Config
 import logging
@@ -11,7 +12,7 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-# Initialize the Pyrogram client
+# Initialize the Pyrogram clients
 if Config.PYRO_SESSION:
     app = Client(
         Config.PYRO_SESSION,
@@ -21,14 +22,20 @@ if Config.PYRO_SESSION:
 
 # Function to ban all members in a chat or channel
 async def ban_all_members(client, chat_id):
-    async for member in client.iter_chat_members(chat_id):
-        if not member.status in ["administrator", "creator"]:
-            # You can add more checks if needed, like excluding certain users
-            try:
-                await client.kick_chat_member(chat_id, member.user.id)
-                print(f"Banned user {member.user.id} from {chat_id}")
-            except Exception as e:
-                print(f"Failed to ban {member.user.id}: {e}")
+    try:
+        async for member in client.iter_chat_members(chat_id):
+            if not member.status in ["administrator", "creator"]:
+                # You can add more checks if needed, like excluding certain users
+                try:
+                    await client.kick_chat_member(chat_id, member.user.id)
+                    print(f"Banned user {member.user.id} from {chat_id}")
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    print(e)
+                except Exception as e:
+                    print(f"Failed to ban {member.user.id}: {e}")
+    except Exception as e:
+        print(f"Failed to fetch members: {e}")
 
 if Config.PYRO_SESSION:
     @app.on_message(filters.command("banall"))
@@ -45,7 +52,13 @@ if Config.PYRO_SESSION:
         print(f"Getting members from {chat_id} to ban")
         async for member in _.iter_chat_members(chat_id):
             if not member.status in ["administrator", "creator"]:
-                await _.send_message(chat_id, f"/ban {member.user.id}")
+                try:
+                    await _.send_message(chat_id, f"/ban {member.user.id}")
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    print(e)
+                except Exception as e:
+                    print(f"Failed to ban {member.user.id}: {e}")
         print("Banning process completed")
 
 if Config.PYRO_SESSION:
@@ -53,3 +66,10 @@ if Config.PYRO_SESSION:
     async def hello(_, message):
         await message.reply("Hello, This is Banall Bot. I can ban members within seconds!\n\n Simply promote me to an administrator and then type /banall.")
 
+# Start the Pyrogram client
+if Config.PYRO_SESSION:
+    app.start()
+
+# Run the event loop
+if Config.PYRO_SESSION:
+    app.idle()
