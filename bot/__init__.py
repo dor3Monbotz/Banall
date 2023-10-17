cde17d43f285711d6b8f71cd2f2b86ee89799685
Gaikwad Kunal <118621248+dor3Monbotz@users.noreply.github.com>
2023-10-18 01:34:30 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,94 +1,55 @@
 import asyncio
-
-from pyrogram.errors.exceptions.flood_420 import FloodWait
-from pyrogram import Client,filters
-from pyrogram.types import *
+from pyrogram import Client, filters
+from pyrogram.types import User, ChatMember
 from .config import Config
 import logging
-from pyrogram.errors import (
-    ChatAdminRequired
-)
+
+# Set up logging
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Initialize the Pyrogram client
 if Config.PYRO_SESSION:
-   ass=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
+    app = Client(
+        Config.PYRO_SESSION,
+        api_id=Config.TELEGRAM_APP_ID,
+        api_hash=Config.TELEGRAM_APP_HASH
+    )
 
-if Config.TELEGRAM_TOKEN:
-   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
-
-if Config.PYRO_SESSION:
-  @ass.on_message(filters.command("banall"))
-  async def _(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
+# Function to ban all members in a chat or channel
+async def ban_all_members(client, chat_id):
+    async for member in client.iter_chat_members(chat_id):
+        if not member.status in ["administrator", "creator"]:
+            # You can add more checks if needed, like excluding certain users
+            try:
+                await client.kick_chat_member(chat_id, member.user.id)
+                print(f"Banned user {member.user.id} from {chat_id}")
+            except Exception as e:
+                print(f"Failed to ban {member.user.id}: {e}")
 
 if Config.PYRO_SESSION:
-  @ass.on_message(filters.command("mbanall"))
-  async def mban(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
+    @app.on_message(filters.command("banall"))
+    async def ban_all_command(_, message):
+        chat_id = message.chat.id
+        print(f"Getting members from {chat_id} to ban")
+        await ban_all_members(_, chat_id)
+        print("Banning process completed")
 
 if Config.PYRO_SESSION:
-  @ass.on_message(filters.command(["start", "ping"]))
-  async def hello(bot: ass, message):
-    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
+    @app.on_message(filters.command("mbanall"))
+    async def mban_all_command(_, message):
+        chat_id = message.chat.id
+        print(f"Getting members from {chat_id} to ban")
+        async for member in _.iter_chat_members(chat_id):
+            if not member.status in ["administrator", "creator"]:
+                await _.send_message(chat_id, f"/ban {member.user.id}")
+        print("Banning process completed")
 
-if Config.TELEGRAM_TOKEN:
-  @bot.on_message(filters.command("banall"))
-  async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.TELEGRAM_TOKEN:
-  @bot.on_message(filters.command("mbanall"))
-  async def mban(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.TELEGRAM_TOKEN:
-  @bot.on_message(filters.command(["start", "ping"]))
-  async def hello(bot, message):
-    await message.reply("Hello, This Is Banall Bot I can Ban Members Within seconds!\n\n Simply Promote my By Adminstration then Type username")
+if Config.PYRO_SESSION:
+    @app.on_message(filters.command(["start", "ping"]))
+    async def hello(_, message):
+        await message.reply("Hello, This is Banall Bot. I can ban members within seconds!\n\n Simply promote me to an administrator and then type /banall.")
 
